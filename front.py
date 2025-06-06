822f51fb58cc6fac3c2b1bf805d1cf18d15ba907
monem <monemahmadi967@gmail.com>
2025-06-06 23:20:24 +0330

    front: version 2.0

--- front.py
+++ front.py
@@ -1,34 +1,49 @@
 import streamlit as st
 
+
 class JobSearchApp:
     def __init__(self):
         if "page" not in st.session_state:
             st.session_state.page = "home"
 
-    def change_page(self, page_name: str) -> None:
+    def change_page(self, page_name):
         st.session_state.page = page_name
 
     def render_home_page(self):
-        st.title("Hello!")
+        st.title("LINKEDIN")
         st.subheader("This app will help you search and find your suit job.")
         st.header("")
+        st.header("Sign in")
+        email = st.text_input("Email", "")
+        password = st.text_input("Password", "")
+        with open("user_email.txt", 'w') as f:
+            f.write(f"{email}"+ '\n')
+            f.write(f"{password}"+ '\n')
         if st.button("ENTER"):
             self.change_page("app_page")
+        
+
 
     def render_app_page(self):
+        
         st.subheader("JOBS")
-        num_results = st.number_input("How many results: ", value=5, min_value=1)
         job_query = st.text_input("Enter the job", "")
-        st.button("SEARCH")
 
-        if st.checkbox("FILTER"):
-            if st.checkbox("LOCATION"):
-                location = st.text_input("City", "")
-            if st.checkbox("EXPERIENCE"):
-                experience_level = st.selectbox(
-                    "EXPERIENCE LEVEL",
-                    ["Internship", "Entry level", "Associate", "Mid-Senior level", "Director", "Executive"]
-                )
+        location = st.text_input("LOCATION", value="Remote")
+
+        experience_level = st.selectbox(
+            "EXPERIENCE LEVEL",
+            ["Internship", "Entry level", "Associate", "Mid-Senior level", "Director", "Executive"]
+        )
+
+        num_results = st.number_input("How many results: ", value=5, min_value=1, max_value=10)
+
+        with open("user_input.txt", 'w') as f:
+            f.write(f"{job_query}"+ '\n')
+            f.write(f"{location}"+ '\n')
+            f.write(f"{experience_level}"+ '\n')
+            f.write(f"{num_results}"+ '\n')
+        st.button("SEARCH", key="search2")
 
         st.write("")
         st.write("")
@@ -40,6 +55,8 @@ class JobSearchApp:
             self.render_home_page()
         elif st.session_state.page == "app_page":
             self.render_app_page()
+
+
 
 if __name__ == "__main__":
     app = JobSearchApp()
