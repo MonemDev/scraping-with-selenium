ec0c1a94842ea8fff3e2e4f9c6e74dfa518ef420
monem <monemahmadi967@gmail.com>
2025-08-14 18:50:06 +0330

    changes in just.py

--- just.py
+++ just.py
@@ -19,6 +19,7 @@ class Linkedin:
         self.driver.get("https://linkedin.com/login")
 
     def login_in_site(self):
+
         self.wait.until(EC.presence_of_element_located((By.ID, "username")))
         with open("user_email.txt", 'r') as f1:
             self.email = f1.readline().strip()
@@ -29,6 +30,11 @@ class Linkedin:
         username_field.send_keys(f"{self.email}")
         password_field.send_keys(f"{self.password}")
         password_field.send_keys(Keys.RETURN)
+        
+        WebDriverWait(self.driver, 180).until(EC.presence_of_element_located((By.ID, "global-nav-search")))
+        
+
+
 
     def searching(self):
         self.wait.until(EC.presence_of_element_located((By.ID, "global-nav-search")))
@@ -39,10 +45,9 @@ class Linkedin:
             self.title = f2.readline().strip()
             self.location = f2.readline().strip()
             self.experience = f2.readline().strip()
-            self.num = f2.readline().strip()
+            self.num = int(f2.readline().strip())
+        
 
-        
-        
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[aria-label='City, state, or zip code']")))
         location_search = self.driver.find_element(By.CSS_SELECTOR, "input[aria-label='City, state, or zip code']")
         location_search.clear()
@@ -58,58 +63,82 @@ class Linkedin:
         job_search.send_keys(Keys.RETURN)
         time.sleep(2)
 
+        WebDriverWait(self.driver, 100).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[aria-label='Search by title, skill, or company']")))
         search_b = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[aria-label='Search by title, skill, or company']")))
         search_b.send_keys(Keys.RETURN)
         time.sleep(10)
-
-    def scroller(self):
-        start = time.time()
-        initialScroll = 0
-        finalScroll = 1000
-
-        while True:
-            self.driver.execute_script(f"window.scrollTo({initialScroll},{finalScroll})")
-            initialScroll = finalScroll
-            finalScroll += 1000
-
-            time.sleep(5)
-
-            end = time.time()
-
-            if round(end - start) > 20:
-                break
+    
+  
 
     def data_mining(self):
-        job_titles = []
+        jobs = self.wait.until(EC.presence_of_all_elements_located(
+            (By.XPATH, "//li[contains(@class, 'occludable-update') and contains(@class, 'scaffold-layout__list-item')]")
+        ))
+        titles = []
         company_names = []
         locations = []
-        links = []
+        types = []
+        links= []
+        existing_titles = set()
+        index = 0
         
-        result = self.driver.find_elements(By.CLASS_NAME, "jobs-search-results__list_item")
-
-        for i in result:
-            if len(job_titles) >= int(self.num):
-                break
+        time.sleep(5)
+        number_job = 1
+        while len(titles)< self.num:
             
-            title = i.find_element(By.CSS_SELECTOR, "a.job-card-list__title--link").get_attribute("aria-label")
-            company = i.find_element(By.CSS_SELECTOR, "div.artdeco-entity-lockup__subtitle").text
-            location = i.find_element(By.CSS_SELECTOR, "div.artdeco-entity-lockup__caption").text
-            link = i.find_element(By.CSS_SELECTOR, "a.job-card-list__title--link").get_attribute("href")
+            jobs = self.wait.until(EC.presence_of_all_elements_located(
+            (By.XPATH, "//li[contains(@class, 'occludable-update') and contains(@class, 'scaffold-layout__list-item')]")
+        ))
+            try :
+                job=jobs[index]
+            except IndexError:
+                print(f"errored jobs done or something happening")
+                
+                break
 
 
-            job_titles.append(title)
-            company_names.append(company)
-            locations.append(location)
-            links.append(link)
+            try:
+                self.driver.execute_script("arguments[0].scrollIntoView();", job)
+                time.sleep(1)
+
+                title = job.find_element(By.CSS_SELECTOR, "a.job-card-container__link").get_attribute("aria-label")
+                if title in existing_titles:
+                    index+=1
+                    continue
+                existing_titles.add(title)
+
+                company = job.find_element(By.CSS_SELECTOR, "div.artdeco-entity-lockup__subtitle").text
+                location = job.find_element(By.CSS_SELECTOR, "div.artdeco-entity-lockup__caption").text
+                link = job.find_element(By.CSS_SELECTOR, "a.job-card-list__title--link").get_attribute("href")
+
+                loc, type_ = location.split("(")
+                type_ = type_[:-1]
+
+                titles.append(title)
+                company_names.append(company)
+                locations.append(loc)
+                types.append(type_)
+                links.append(link)
+                index += 1
+                self.driver.execute_script("window.scrollBy(0, 150);")
+                time.sleep(1)
+
+                print("job",number_job,": success full")
+                number_job += 1
+
+            except Exception as e:
+                print(e)
+                continue
 
         df = pd.DataFrame({
-            'Title': job_titles,
+            'Title': titles,
             'Company': company_names,
             'Location': locations,
+            'Types': types,
             'Link': links
         })
 
-        df.to_csv('result.csv', index=False)
+        df.to_csv('result.csv', index=False, encoding='utf-8')
 
     def quit(self):
         self.driver.close()
@@ -118,7 +147,6 @@ class Linkedin:
         self.open_site()
         self.login_in_site()
         self.searching()
-        self.scroller()
         self.data_mining()
         self.quit()
 
