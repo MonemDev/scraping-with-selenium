71c095cb949f9e5e9dd4de607f57645a1f1685c6
monem <monemahmadi967@gmail.com>
2025-08-15 18:45:16 +0330

    compeleted

--- front1.py
+++ front1.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import pandas as pd
+from just import *
 
 class JobSearchApp:
     def __init__(self):
@@ -11,7 +12,7 @@ class JobSearchApp:
 
     def render_home_page(self):
         st.title("LINKEDIN")
-        st.subheader("This app will help you search and find your suit job.")
+        st.subheader("This app will help you search and find suit job for you.")
         st.header("")
         st.header("Sign in")
         email = st.text_input("Email", "")
@@ -23,39 +24,45 @@ class JobSearchApp:
             self.change_page("app_page")
         
 
-
     def render_app_page(self):
         
-        st.subheader("JOBS")
-        job = st.text_input("Enter the job", "")
+        
+        job = st.text_input("JOBS", "Data Science")
 
-        location = st.text_input("LOCATION", value="Remote")
+        location = st.text_input("LOCATION", value="United State")
 
         experience_level = st.selectbox(
             "EXPERIENCE LEVEL",
             ["Internship", "Entry level", "Associate", "Mid-Senior level", "Director", "Executive"]
         )
 
-        num_results = st.number_input("How many results: ", value=5, min_value=1, max_value=10)
+        num_results = st.number_input("How many results: ", value=10, min_value=1, max_value=20)
 
         with open("user_input.txt", 'w') as f:
             f.write(f"{job}"+ '\n')
             f.write(f"{location}"+ '\n')
             f.write(f"{experience_level}"+ '\n')
             f.write(f"{num_results}"+ '\n')
-        st.button("SEARCH", key="search2")
+        if st.button("SEARCH"):
+            self.change_page("waiting_page")
 
-        st.write("")
-        st.write("")
-        if st.button("RETURN HOME"):
-            self.change_page("home")
+
+    def render_waiting_page(self):
+        st.subheader("Result")
+        
+        Data = Linkedin()
+        Data.run()
+        df = pd.read_csv("result.csv")
+        st.dataframe(df)
+        
 
     def run(self):
         if st.session_state.page == "home":
             self.render_home_page()
         elif st.session_state.page == "app_page":
             self.render_app_page()
-
+        elif st.session_state.page == "waiting_page":
+            self.render_waiting_page()
 
 
 if __name__ == "__main__":
